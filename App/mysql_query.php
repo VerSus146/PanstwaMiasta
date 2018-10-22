df51d6c6d53ea1b26dfa9667eb75bd1636b22cb3
sylek38 <sylek38@gmail.com>
2018-10-22 12:35:08 +0200

    Zmiany w plikach

--- App/mysql_query.php
+++ App/mysql_query.php
@@ -6,23 +6,24 @@ $data = $_POST['data'];
 
 $query = $_POST['query'];
 
+
+$correct=false;
+
 $result;
 
 $con = mysqli_connect($host, $db_user, $db_password, $db_name) or die("Błąd podczas łączenia do bazy");
 
 mysqli_query($con, "SET NAMES utf8");
 
-/*switch($query){
+switch($query){
     case "Kolor":
-        $result=mysqli_query($con,"SELECT kolory FROM kolory");
+        $result=mysqli_query($con,"SELECT kolory FROM kolory") or die("Zle zapytanie o kolory");
         break;
         
-    case "Pañstwo":
-        
-}*/
-$result=mysqli_query($con,"SELECT Panstwa FROM panstwa");
-
-$correct=false;
+    case "Państwo":
+        $result=mysqli_query($con,"SELECT Panstwa FROM panstwa") or die ("Zle zapytanie o panstwo");
+        break;
+}
 
 while ($row = $result->fetch_assoc()) {
     if($data == $row['Panstwa']){
--- App/player_input.js
+++ App/player_input.js
@@ -3,6 +3,8 @@ function send_answer(){
     let data = document.getElementById("answer").value;
     let query = document.getElementById("question").innerHTML;
 
+    console.log("Chuj kurwa")
+
     var httpc = new XMLHttpRequest();
     var url = "mysql_query.php";
     
@@ -16,6 +18,7 @@ function send_answer(){
         if(httpc.readyState == 4 && httpc.status == 200) {
             switch(httpc.responseText){
                 case "correct":
+                    console.log("Dziala");
                     count_points(data);
                     break;
                     
@@ -31,8 +34,6 @@ function send_answer(){
         var pointsDiv = document.getElementById("points");
         var pointsToAdd = parseInt(word.replace(/\s+/g, '').length);
         var actualPoints = parseInt(pointsDiv.innerHTML);
-        console.log(actualPoints);
-        console.log(pointsToAdd + actualPoints);
         var newPointsNumber = pointsToAdd + actualPoints;
         
         pointsDiv.innerHTML = newPointsNumber;
